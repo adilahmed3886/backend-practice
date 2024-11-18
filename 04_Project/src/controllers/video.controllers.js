67264a96b00cdf001e94cfb28b4ad88859b760ba
adil <adilahmed3886@gmail.com>
2024-11-18 12:12:29 +0500

    add video endpoint

---
+++ 04_Project/src/controllers/video.controllers.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.models.js";
+import { uploadToCloudinary } from "../utils/cloudinary.upload.js";
+import  {Video} from "../models/video.models.js"
+
+const publishVideo = asyncHandler(async(req, res) => {
+    const {title, description} = req.body;
+    if(!title && !description){
+        throw new ApiError(400, "Provide a title along with description")
+    }
+
+    const user = await User.findById(req.user._id)
+    if(!user){
+        throw new ApiError(400, "You have to be a user to publish video")
+    }
+
+    const videoPath = req.files?.videoFile[0]?.path
+    const thumbnailPath = req.files?.thumbnail[0]?.path
+    const uploadedVideo = await uploadToCloudinary(videoPath)
+    const uploadedThumbnail = await uploadToCloudinary(thumbnailPath)
+
+    if(!uploadedVideo && !uploadedThumbnail){
+        throw new ApiError(500, "Failed to upload files to cloudinary")
+    }
+    console.log(uploadedVideo)
+
+    const video = await Video.create({
+        title,
+        description,
+        duration: uploadedVideo.duration,
+        videoFile: uploadedVideo.secure_url,
+        thumbnail: uploadedThumbnail.secure_url,
+        owner: req.user._id,
+        isPublished: false
+    })
+
+    const videoUploaded = await Video.findById(video._id);
+    if(!videoUploaded){
+        throw new ApiError(500, "Failed to upload video to DB")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Video uploaded Succefully"))
+
+
+})
+
+const updateVideo = asyncHandler(async(req, res) => {})
+
+const deleteVideo = asyncHandler(async(req, res) => {
+    const {videoId} = req.params;
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Provide a valid video id")
+    }
+
+    const video = await Video.findById(videoId);
+    if(!video){
+        throw new ApiError(404, "Video not found")
+    }
+
+    if(video?.owner.toString() !== req.user._id.toString()){
+        throw new ApiError(400, "Only owner can delete their video")
+    }
+
+    const deletedVideo = await Video.findByIdAndDelete(videoId);
+
+    if(!deletedVideo){
+        throw new ApiError(500, "Failed to delete video")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Video deleted successfully"))
+})
+
+const getVideoById = asyncHandler(async(req, res) => {})
+
+const togglePublishStatus = asyncHandler(async(req, res) => {})
+
+const getAllVideos = asyncHandler(async (req, res) => {})
+
+export {
+    getAllVideos,
+    publishVideo,
+    getVideoById,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus
+}
+
